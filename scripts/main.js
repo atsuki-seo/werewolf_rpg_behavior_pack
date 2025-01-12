f22bbd4cb72c4f3470568be62c79c4add952e0fe
Atsuki Seo <atsuki.seo.name@gmail.com>
2025-01-13 00:43:09 +0900

    サンプルアイテム追加

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,6 @@
 import { world, system } from "@minecraft/server";
 
 function mainTick() {
-    if (system.currentTick % 200 === 0) {
-        world.sendMessage("[SYSTEM] Welcome to 人狼RPG WORLD");
-    }
     system.run(mainTick);
 }
 
